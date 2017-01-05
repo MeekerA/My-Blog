4fd7207c0d4cd225be3f0d748e5f8c9ab00b1808
Austin Meeker <austinmeeker@me.com>
2017-01-05 09:28:15 -0600

    Added Partials to dice game.

--- src/main/java/com/codeup/DiceGameController.java
+++ src/main/java/com/codeup/DiceGameController.java
@@ -19,9 +19,12 @@ public class DiceGameController {
 
     @GetMapping("/roll-dice/{n}")
     public String showResult(@PathVariable int n, Model model) {
+
         int randomNumber = 1 + (int)(Math.random() * 6);
+
         model.addAttribute("randomNumber", randomNumber);
         model.addAttribute("number", n);
+
         return "diceGame";
     }
 }
